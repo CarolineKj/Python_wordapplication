0f3263453e66664188e421dac7a276bd1f42fdd0
Caroline Kjellander <cakj2001@student.miun.se>
2023-02-01 12:44:24 +0100

    Modified consonant count

--- Python_wordapplication/Python_wordapplication.py
+++ Python_wordapplication/Python_wordapplication.py
@@ -4,18 +4,19 @@ from tkinter import ttk
 
 words = "" 
 win = tk.Tk()
-win.title("Word Converter App")
+win.title("Sentance Converter App")
 lbl = ttk.Label(win, text = "Skriv in en mening:", anchor='w').grid(column = 0, row = 0) #anchoring fungerar inte
 win.geometry("450x300")
 
 def click():   
     #följande labels för att skriva ut svaret i widget
-    lbl2 = ttk.Label(win, anchor="w", text = "Ursprunglig textsträng: " + words.get()).grid(column = 0, row = 3)
-    lbl3 = ttk.Label(win, text = "Omvänd ordning på orden: " + reverseOrder(words.get())).grid(column = 0, row = 4)
-    lbl4 = ttk.Label(win, text = "Antal konsonanter: " + str(countConsonants(words.get()))).grid(column = 0, row = 5)
-    lbl5 = ttk.Label(win, text = "Antal ord: " + str(len(words.get().split()))).grid(column = 0, row = 6)
+    ttk.Label(win, anchor="w", text = "Ursprunglig textsträng: " + words.get()).grid(columns = 2, row = 3)
+    ttk.Label(win, text = "Omvänd ordning på orden: " + reverseOrder(words.get())).grid(columns = 2, column = 0, row = 4)
+    ttk.Label(win, text = "Antal ord: " + str(len(words.get().split()))).grid(columns =2 , column = 0, row = 6)
+    ttk.Label(win, text = "Antal konsonanter: " + str(countConsonants(words.get()))).grid(columns = 2, column = 0, row = 5)
+    
 
-    #följande prints för att skriva ut i textrutan istället för i widgeten
+    #följande prints för att skriva ut i prompten istället för i widgeten
     #print("Ursprunglig textsträng: " + name.get()) 
     #print("Omvänd ordning på orden: " + reverseOrder(name.get()))
     #print("Antal konsonanter: " + str(countConsonants(name.get())))
@@ -23,8 +24,8 @@ def click():
 
 def countConsonants(string):
    num_consonants = 0
-   for char in string:
-      if char not in "aeiouAEIOU ,.;:!?": #kan lägga till fler tecken här så klart 
+   for char in string.lower():
+      if char in "bcdfghjklmnpqrstvwxyz": 
          num_consonants += 1
    return num_consonants
 
